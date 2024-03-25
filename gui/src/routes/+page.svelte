b640fcd8ab602c3072898ba6b7f1602316e5ce35
Nikita Khvorostin <khvorostinn@gmail.com>
2024-03-26 01:46:44 +0300

    Setup project

---
+++ gui/src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  let counter = $state(0)
+
+  function inc() {
+    ++counter
+  }
+
+  function dec() {
+    counter -= 1
+  }
+</script>
+
+<main>
+  <h1>Counter: {counter}</h1>
+  <div class="buttons">
+    <button onclick={inc}>+</button>
+    <button onclick={dec}>-</button>
+  </div>
+</main>
+
+<style>
+  button {
+    font-size: 40px;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    gap: 100px;
+  }
+
+  main {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
